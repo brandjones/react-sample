ccd7d6ccfb6eec16ab9fac6ba43f028ad5212f80
Brandon Jones <Brandon_Jones@brandons-air.home>
2018-02-18 11:21:51 -0500

    setup public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,23 +3,22 @@ import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashBoardPage from '../components/ExpenseDashBoardPage';
 import LoginPage   from '../components/LoginPage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/HelpPage';
-import { Router, Route, Switch, Link, NavLink} from 'react-router-dom';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute  from './PublicRoute';
 
 export const history = createHistory();
 const AppRouter = () => (
   <Router history={history}>
       <div>
         <Switch>
-          <Route path='/'           component={LoginPage} exact={true}/>
+          <PublicRoute path='/'            component={LoginPage} exact={true}/>
           <PrivateRoute path='/dashboard'  component={ExpenseDashBoardPage} />
           <PrivateRoute path='/create'     component={AddExpensePage} />
           <PrivateRoute path='/edit/:id'   component={EditExpensePage} />
-          <Route path='/help'       component={HelpPage} />
-          <Route                    component={NotFoundPage} />
+          <Route                           component={NotFoundPage} />
         </Switch>
       </div>
   </Router>
